84488d41c767466939d2b2de4bd26d78c7c204e2
Adrian-Adame <aadame7@daltonstate.edu>
2026-01-22 13:11:35 -0500

    Add files via upload

---
+++ OperatorExample.java
@@ -0,0 +1,26 @@
+public class OperatorExample
+{
+      public static void main(String[] args)
+      {
+         System.out.println(3 + 7);
+         System.out.println(3 - 7);
+         System.out.println(3 * 7);
+         
+         System.out.println(3.0 + 7.0);
+         System.out.println(3.0 - 7.0);
+         System.out.println(3.0 * 7.0);
+         
+         //interger divisiom
+         System.out.println(10 / 3);
+         System.out.println(10.0/3.0);
+         //System.out.println(1/0);
+         
+         System.out.println(117 % 4);
+         System.out.println(9 % 3);
+         System.out.println(2 % 7);
+         
+         //multiplication done first
+         System.out.println( 7+ 5 * 3);
+         System.out.println( (7 + 5) * 3);
+       }
+}
