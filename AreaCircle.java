ab6a8f59150a5cd56ae083b1173f30f78d7a9555
Adrian-Adame <aadame7@daltonstate.edu>
2026-01-22 13:39:08 -0500

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,12 @@
+public class AreaCircle
+{
+   public static void main(String[] args)
+   {
+      double radius = 15.3;
+      
+      double area = 3.1415 * radius * radius;
+      
+      System.out.println("the area of the circle with radius " + radius + 
+         " units is equal to "  + area + " square units ");
+   }
+}
