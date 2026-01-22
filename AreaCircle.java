03b4d5a5c8282726840dd7076046b416324dd425
Adrian-Adame <aadame7@daltonstate.edu>
2026-01-22 13:41:17 -0500

    Add files via upload

--- AreaCircle.java
+++ AreaCircle.java
@@ -4,7 +4,7 @@ public class AreaCircle
    {
       double radius = 15.3;
       
-      double area = 3.1415 * radius * radius;
+      double area = Math.PI * Math.pow(radius,2.0);
       
       System.out.println("the area of the circle with radius " + radius + 
          " units is equal to "  + area + " square units ");
