1d61472db12952534467ac6656e2d311f62cae1d
Adrian-Adame <aadame7@daltonstate.edu>
2026-01-22 13:32:08 -0500

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,11 @@
+public class AreaCircle
+{
+   public static void main(String[] args)
+   {
+      double radius = 5.3;
+      
+      double area = 3.1415 * radius * radius;
+      
+      System.out.println(area);
+   }
+}
